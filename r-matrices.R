e2447c733cf34c7d8af9f22bc9914e51f968657c
jwonyk <wonyoungkim@ucsb.edu>
2025-08-07 11:14:12 -0700

    start project

--- r-matrices.R
+++ r-matrices.R
@@ -1,5 +1,5 @@
-# Make a sequence of values from 1 - 10 ----
+# Make a sequence of values from 1 - 10
 my_values <- seq(from = 1, to = 10, by = 1)
 
-# Look at it (always) ----
+# Look at it (always)
 my_values
